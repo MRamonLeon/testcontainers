d73e5fe27abb723f8da83af9805e096de3826c9d
Ramon Leon <manuelramonleonjimenez@gmail.com>
2020-06-09 09:59:30 +0200

    Foundation marker and pipeline files

--- src/test/java/com/rleon/testcontainers/TestContainersTest.java
+++ src/test/java/com/rleon/testcontainers/TestContainersTest.java
@@ -1,4 +1,18 @@
 package com.rleon.testcontainers;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.Assert.*;
+
 public class TestContainersTest {
+    @Rule
+    public GenericContainer container = new GenericContainer<>("alpine:latest");
+    
+    @Test
+    public void testItWorks() {
+        assertNotNull(container.getHost());
+        System.out.println("Alpine started in host: " + container.getHost());
+    }
 }
