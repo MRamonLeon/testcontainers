b6b5283c7832364f4f5ba7eff9d4937da5941cf4
Ramon Leon <manuelramonleonjimenez@gmail.com>
2020-06-12 09:40:07 +0200

    wit fixture

--- src/test/java/com/rleon/testcontainers/DockerFixtureTest.java
+++ src/test/java/com/rleon/testcontainers/DockerFixtureTest.java
@@ -9,15 +9,17 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class DockerFixtureTest {
-    //@Rule
-    public DockerRule<SquidContainer> rule = new DockerRule<SquidContainer>(SquidContainer.class);
+    @Rule
+    public DockerRule<SquidContainer> rule = new DockerRule<>(SquidContainer.class);
     private SquidContainer container;
     
-    //@Test
+    @Test
     public void dockerContainerTest() throws IOException, InterruptedException {
         container = rule.get();
+        System.out.print("***** SQUID LOGS AT DOCKER FIXTURE *****");
         System.out.println(new String(Files.readAllBytes(container.getLogfile().toPath()), StandardCharsets.UTF_8));
         System.out.print("\n\n\n");
+        System.out.print("***** SQUID ACCESS LOGS AT DOCKER FIXTURE *****");
         System.out.println(container.getAccessLog());
     }
 }
